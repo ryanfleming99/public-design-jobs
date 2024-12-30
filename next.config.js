82f11c4c196c0546f69c61a5cb2466a6939fe816
Ryan Fleming <ryanfleming93@aol.com>
2024-12-30 12:29:25 +0000

    added netlify file, updated config for deployment

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  distDir: "build" // Ensure this matches the directory you intend to use
+  distDir: "build",
+  trailingSlash: true // Ensure this matches the directory you intend to use
 };
